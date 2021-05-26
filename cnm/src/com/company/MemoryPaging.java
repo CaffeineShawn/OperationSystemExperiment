d665a8361a543ae26bae6f4b6c3ad3f33e9f04e8
CaffeineShawn <52215406+CaffeineShawn@users.noreply.github.com>
2021-05-26 23:01:19 +0800

    OPT

--- cnm/src/com/company/MemoryPaging.java
+++ cnm/src/com/company/MemoryPaging.java
@@ -6,47 +6,81 @@ import java.util.*;
 public class MemoryPaging {
     static LinkedList<Integer> pageInMemoryFIFO = new LinkedList<>();
     static LinkedList<Integer> pageInMemoryLRU = new LinkedList<>();
+    static LinkedList<Integer> pageInMemoryOPT = new LinkedList<>();
     static int memoryBlock = 0;
 
     static int missingPageFIFO = 0;
     static int missingPageLRU = 0;
+    static int missingPageOPT = 0;
     static int timesCount = 0;
 
 
 
 
     public static void main(String[] args) {
-        boolean[] instructionsArrayFIFO = new boolean[320];
-        boolean[] instructionsArrayLRU = new boolean[320];
+        int[] instructionsArray = new int[321];
+
         memoryBlock = 3;
 //        System.out.print("内存块数: ");
 //        memoryBlock = new Scanner(System.in).nextInt();
 //        while (!checkAllDone(instructionsArray)){
+        int j = 0;
         for (int i = 0; i < 64; i++) {
             int m = (int) (Math.random() * 320);
-            if (m + 1 <= 319){
-                instructionExecute(instructionsArrayFIFO,instructionsArrayLRU,m+1);
-            }
+//            if (m + 1 <= 319){
+//                instructionExecute(m+1);
+//            }
+            instructionsArray[j] = m + 1;
+            ++j;
             int m1 = (int) (Math.random() * (m - 1));
-//            System.out.println("pick: m1  =" + m1);
-            instructionExecute(instructionsArrayFIFO,instructionsArrayLRU,m1);
 
-//            System.out.println("pick: m1+1=" + (m1 + 1));
-            instructionExecute(instructionsArrayFIFO,instructionsArrayLRU,m1+1);
-
+//            instructionExecute(m1);
+            instructionsArray[j] = m1;
+            ++j;
+//            instructionExecute(m1+1);
+            instructionsArray[j] = m1+1;
+            ++j;
             int m2 = (int) (Math.random() * (320 - (m1 + 2)) + m1 + 2);
-//            System.out.println("pick: m2  =" + m2);
 
-            instructionExecute(instructionsArrayFIFO,instructionsArrayLRU,m2);
 
-            if (m2 + 1 <= 319) {
-//                System.out.println("pick: m2+1=" + (m2 + 1));
-                instructionExecute(instructionsArrayFIFO,instructionsArrayLRU,m2+1);
-
-            }
+//            instructionExecute(m2);
+            instructionsArray[j] = m2;
+            ++j;
+//            if (m2 + 1 <= 319) {
+//
+//                instructionExecute(m2+1);
+//
+//            }
+            instructionsArray[j] = m2+1;
+            ++j;
 
 
         }
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 1; i < 321; i++) {
+            System.out.printf("%d\t",instructionsArray[i]);
+            if (i % 5 == 0) {
+                System.out.printf("\n");
+            }
+            if (map.containsKey(instructionsArray[i]/10)) {
+                map.put((instructionsArray[i]/10), map.get(instructionsArray[i]/10) + 1);
+            } else {
+                map.put((instructionsArray[i]/10), 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+
+        }
+
+
+
+        for (int i = 1; i < 321; i++) {
+            instructionExecute(instructionsArray[i],map);
+        }
+
 
 //        System.out.println("Times of missing page:" + missingPageFIFO);
         System.out.printf("FIFO缺页率: %f", ((float) missingPageFIFO / (float) timesCount) * 100);
@@ -56,16 +90,20 @@ public class MemoryPaging {
         System.out.printf("LRU缺页率: %f", ((float) missingPageLRU / (float) timesCount) * 100);
         System.out.println("%");
         System.out.println("LRU缺页次数: "+missingPageLRU );
+        System.out.printf("OPT缺页率: %f", ((float) missingPageOPT / (float) timesCount) * 100);
+        System.out.println("%");
+        System.out.println("OPT缺页次数: "+missingPageOPT );
+
     }
 
-    static void instructionExecute(boolean[] FIFO,boolean[] LRU, int index) {
+    static void instructionExecute(int index,Map<Integer,Integer> map) {
         int pageIndex = index / 10;
 
 
         System.out.println("------Currently executing: " + index + " at Page " + pageIndex);
         FIFO(pageIndex, pageInMemoryFIFO);
         LRU(pageIndex, pageInMemoryLRU);
-
+        OPT(pageIndex,pageInMemoryOPT,map);
 
         timesCount++;
 //        switch (mode) {
@@ -125,27 +163,6 @@ public class MemoryPaging {
                 pageInMemory.add(swapOutIndex, pageIndex);
                 pageInMemory.removeFirstOccurrence(swapOut);
                 notRecentlyUsed.offer(pageIndex);
-//                Map<Integer, Integer> map = new TreeMap<>();
-//                for (int i : pageInMemory) {
-//                    map.put(i, pageInMemory.indexOf(i));
-//                }
-//                int swappedOut = notRecentlyUsed.peekFirst();
-//                for (int i : pageInMemory) {
-//                    if (map.get(swappedOut) > map.get(i)) {
-//                        swappedOut = i;
-//                    }
-//                }
-
-//                int swappedOut = notRecentlyUsed.pollFirst();
-//                while (notRecentlyUsed.contains(swappedOut)) {
-//                    notRecentlyUsed.removeLastOccurrence(swappedOut);
-//                }
-//                System.out.println(swappedOut);
-//                pageInMemory.removeFirstOccurrence(swappedOut);
-//                //System.out.println(swappedOut);
-//                System.out.println("LRU Swap in " + pageIndex);
-//                pageInMemory.addFirst(pageIndex);
-//                notRecentlyUsed.offer(pageIndex);
 
                 System.out.println("LRU swap in " +pageIndex);
 
@@ -172,6 +189,25 @@ public class MemoryPaging {
             System.out.printf("%d ", integer);
         }
         System.out.print("\n");
+    }
+
+    static void OPT(int pageIndex, LinkedList<Integer> pageInMemory,Map<Integer,Integer> map) {
+        if (!pageInMemory.contains(pageIndex)) {
+            missingPageOPT++;
+            if (pageInMemory.size() == memoryBlock) {
+                int lessOppotunity = pageInMemory.peekFirst();
+                for (int i : pageInMemory) {
+                    if (map.get(i) < map.get(lessOppotunity)) {
+                        lessOppotunity = i;
+                    }
+                }
+                pageInMemory.removeFirstOccurrence(lessOppotunity);
+                pageInMemory.offer(pageIndex);
+            } else {
+                pageInMemory.offer(pageIndex);
+            }
+        }
+
     }
 
     static boolean checkAllDone(boolean[] array) {
